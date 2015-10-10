7b4d62f39cb8f9b345926101a0d756dfdc5e1a89
Lance0226 <hl0614020104@126.com>
2015-10-10 10:45:59 +0800

    Spring MVC login demo
    Spring MVC login demo

---
+++ oa_system/OA_System/src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.programcreek.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("hello.do")
+	/*
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	*/
+	public ModelAndView showMessage(
+			@RequestParam(value="username") String username,
+			@RequestParam(value="password") String password
+			)
+	{
+		ModelAndView mv=new ModelAndView("helloworld");
+		mv.addObject("username", username);
+		mv.addObject("password", password);
+		return mv;
+	}
+}
